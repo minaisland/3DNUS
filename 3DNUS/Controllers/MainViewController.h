5a7fd6ba1fc17dccc365a8c99b8e4803a3baf1e0
阿东 <konaod@gmail.com>
2015-12-13 14:17:42 +0800

    加入gitignore和预编译头文件

---
+++ 3DNUS/Controllers/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  3DNUS
+//
+//  Created by 郑先生 on 15/12/13.
+//  Copyright © 2015年 郑先生. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainViewController : NSViewController
+
+@end
---
+++ 3DNUS/Controllers/MainViewController.m
@@ -0,0 +1,22 @@
+//
+//  MainViewController.m
+//  3DNUS
+//
+//  Created by 郑先生 on 15/12/13.
+//  Copyright © 2015年 郑先生. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
