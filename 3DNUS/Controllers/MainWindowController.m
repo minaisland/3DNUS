53f43abac2d1e432b3ee37e2338580188fba7c1f
阿东 <konaod@gmail.com>
2015-12-13 15:21:31 +0800

    关闭按钮退出程序

---
+++ 3DNUS/Controllers/MainWindowController.m
@@ -0,0 +1,29 @@
+//
+//  MainWindowController.m
+//  3DNUS
+//
+//  Created by 郑先生 on 15/12/13.
+//  Copyright © 2015年 郑先生. All rights reserved.
+//
+
+#import "MainWindowController.h"
+
+@interface MainWindowController () <NSWindowDelegate>
+
+@end
+
+@implementation MainWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+- (BOOL)windowShouldClose:(id)sender
+{
+    exit(0);
+    return YES;
+}
+
+@end
